d9a4e3929c061d3ce63ea6db320aeb889dc6b7ed
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-15 10:11:00 +0700

    adding heigth,phone,email property to students_seeder and also seed teachers data

--- seeders/20170412080026-student-seeder.js
+++ seeders/20170412080026-student-seeder.js
@@ -19,6 +19,9 @@ module.exports = {
       last_name: 'Doe',
       birthdate: new Date('1989-03-25'),
       // birthdate: new Date('1989-03-25').toISOString().substring(0, 10),
+      height: 170,
+      phone: '1234567899',
+      email: 'dow@gmail.com',
       createdAt: new Date(),
       updatedAt: new Date()
     },
@@ -26,6 +29,9 @@ module.exports = {
       first_name: 'John',
       last_name: 'Wick',
       birthdate: new Date('1970-05-12'),
+      height: 159,
+      phone: '1234567899',
+      email: 'wick@gmail.com',
       // birthdate: new Date('1970-05-12').toISOString().substring(0, 10),
       createdAt: new Date(),
       updatedAt: new Date()
@@ -33,6 +39,9 @@ module.exports = {
     {
       first_name: 'John',
       last_name: 'Bro',
+      height: 165,
+      phone: '1234567899',
+      email: 'bro@gmail.com',
       birthdate: new Date('1965-11-21'),
       // birthdate: new Date('1965-11-21').toISOString().substring(0, 10),
       createdAt: new Date(),
